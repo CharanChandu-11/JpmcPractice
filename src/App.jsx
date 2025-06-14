851a69e059f2759b30d9e28258fb66a6215b2635
Monika-mls <monikatammineni2175@gmail.com>
2025-06-14 23:31:52 +0530

    Created basic pages outline

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './pages/Home'
+import Blogs from './pages/Blogs'
+import Payment from './pages/Payment'
+import Volunteer from './pages/Volunteer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
